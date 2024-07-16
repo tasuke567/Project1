789bca59a3a33a5e722e6f076bc0e88cb7994608
tasuke567 <80236261+tasuke567@users.noreply.github.com>
2024-07-16 18:19:36 +0700

    Deploy Flask backend to Heroku

--- app.py
+++ app.py
@@ -29,4 +29,5 @@ def predict():
     return jsonify({'prediction': prediction.tolist()})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
