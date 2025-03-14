e65df74b2b4f675079395e57da92733b01c63ebd
tasuke567 <80236261+tasuke567@users.noreply.github.com>
2025-03-14 21:32:36 +0700

    Comment out Login button in NavBar component

--- my-react-app/src/NavBar.jsx
+++ my-react-app/src/NavBar.jsx
@@ -18,7 +18,7 @@ function NavBar({ isAuthenticated, handleLogout, toggleLoginForm }) {
               onClick={toggleLoginForm}
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
-              Login
+              {/* Login */}
             </button>
           )}
         </div>
